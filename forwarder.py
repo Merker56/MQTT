6a340c6a0f7e58088b1e077df218d93e00b4b69c
Merker56 <Merker56@users.noreply.github.com>
2019-09-14 11:36:41 -0400

    Add files via upload

--- forwarder.py
+++ forwarder.py
@@ -10,12 +10,16 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     #Receives the message and forwards it to cloud
-    client.publish("Forward",payload=msg.payload,qos=2,retain=False)
+    client.publish("Forward/#",payload=msg.payload,qos=2,retain=False)
+    print("Published message to Forward!")
     
+def on_disconnect(client, userdata):
+    print("Disconnected")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_disconnect = on_disconnect
 
 client.connect("174.130.55.179", 1883, 60)
 # full connect example
--- receiver.py
+++ receiver.py
@@ -5,17 +5,13 @@ import datetime
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
     client.subscribe("Forward/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
     #Gets the image and passes to Saver
-    client.publish("Receive",payload=msg.payload,qos=2,retain=False)
+    client.publish("Receive/#",payload=msg.payload,qos=2,retain=False)
 
 client = mqtt.Client()
 client.on_connect = on_connect
