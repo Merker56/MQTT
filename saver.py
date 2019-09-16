dc309fc78612d6d22e2e1f6d315600a9c32bbbbb
Merker56 <Merker56@users.noreply.github.com>
2019-09-16 14:56:28 -0400

    Update saver.py

--- saver.py
+++ saver.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import os
 import time
 
+i = 0
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -11,7 +12,8 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     print(msg.topic+" message received!")
     #Gets the image and saves it to disk
-    f=open("/data/" + time.time() + ".png", "w")
+    i = i + 1
+    f=open("/data/face_" + i + ".png", "w")
     print("File Opened!")
     f.write(msg.payload)
     print("Content written")
